698adad21f37834c589acad5161acedcd5941e44
Patrick Romão <patrickromao2001@gmail.com>
2024-01-14 13:27:56 -0300

    remove unnecessary

--- src/Boundaries/SearchAuthors.cs
+++ src/Boundaries/SearchAuthors.cs
@@ -7,7 +7,7 @@ public class SearchAuthors
     [FromQuery(Name = "searchTerm")]
     public string? SearchTerm { get; init; } = "";
     [FromQuery(Name = "skip")]
-    public long? Skip { get; init; } = 0;
+    public long? Skip { get; init; }
     [FromQuery(Name = "take")]
-    public long? Take { get; init; } = 10;
+    public long? Take { get; init; }
 }
